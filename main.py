4a4187879989e22acb4c09e248c36fc118fcc504
nick-ob <188435909+nick-ob@users.noreply.github.com>
2026-05-25 19:05:05 +0200

    Added snake moving and first input handling to move snake

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ def draw_snake(snake: list[tuple[int, int]], block_size) -> None:
         rect = pg.Rect(x * block_size, y * block_size, block_size, block_size)
         pg.draw.rect(screen, (50, 200, 50), rect)
 
+def move_snake(snake: list[tuple[int, int]], direction: tuple[int, int]) -> list[tuple[int, int]]:
+    snake_moved: list[tuple[int, int]] = []
+
+    x_now, y_now = snake[0]
+    x_mov, y_mov = direction
+    snake_moved.append((x_now + x_mov, y_now + y_mov))
+
+    for part in snake[:-1]:
+        snake_moved.append(part)
+
+    return snake_moved
+
 pg.init()
 
 width = 800
@@ -26,17 +38,29 @@ clock = pg.time.Clock()
 
 field_size: tuple[int, int] = (width // block_size, heigth // block_size)
 snake: list[tuple[int, int]] = [(int((width / block_size) / 2), int((heigth / block_size) / 2))]
+direction: tuple[int, int] = (1, 0)
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_k and direction != (0, 1):
+                direction = (0, -1)
+            if event.key == pg.K_j and direction != (0, -1):
+                direction = (0, 1)
+            if event.key == pg.K_h and direction != (1, 0):
+                direction = (-1, 0)
+            if event.key == pg.K_l and direction != (-1, 0):
+                direction = (1, 0)
+
     draw_grid(screen, width, heigth, block_size)
     draw_snake(snake, block_size)
+    snake = move_snake(snake, direction)
 
     pg.display.flip()
-    clock.tick()
+    clock.tick(10)
 
 pg.quit()
 sys.exit()
