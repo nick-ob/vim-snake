06b12fe7e3010a5b95fd1fbc6a343590f6d37a05
nick-ob <188435909+nick-ob@users.noreply.github.com>
2026-05-25 20:11:54 +0200

    Added food spawning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def draw_grid(screen, width: int, heigth: int, block_size: int) -> None:
     screen.fill((50, 50, 50))
@@ -25,6 +26,17 @@ def move_snake(snake: list[tuple[int, int]], direction: tuple[int, int]) -> list
 
     return snake_moved
 
+def draw_food(screen, coords: tuple[int, int], block_size: int) -> None:
+    x, y = coords
+    rect = pg.Rect(x * block_size, y * block_size, block_size, block_size)
+    pg.draw.rect(screen, (200, 50, 50), rect)
+
+def random_coord(field_size: tuple[int, int]) -> tuple[int, int]:
+    x = random.randint(0, field_size[0] - 1)
+    y = random.randint(0, field_size[1] - 1)
+
+    return (x, y)
+
 pg.init()
 
 width = 800
@@ -39,6 +51,7 @@ clock = pg.time.Clock()
 field_size: tuple[int, int] = (width // block_size, heigth // block_size)
 snake: list[tuple[int, int]] = [(int((width / block_size) / 2), int((heigth / block_size) / 2))]
 direction: tuple[int, int] = (1, 0)
+food: tuple[int, int] = random_coord(field_size)
 
 while running:
     for event in pg.event.get():
@@ -55,9 +68,14 @@ while running:
             if event.key == pg.K_l and direction != (-1, 0):
                 direction = (1, 0)
 
+    if snake[0] == food:
+        food = random_coord(field_size)
+        print(food)
+
+    snake = move_snake(snake, direction)
     draw_grid(screen, width, heigth, block_size)
     draw_snake(snake, block_size)
-    snake = move_snake(snake, direction)
+    draw_food(screen, food, block_size)
 
     pg.display.flip()
     clock.tick(10)
